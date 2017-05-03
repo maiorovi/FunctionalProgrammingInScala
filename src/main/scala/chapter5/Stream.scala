0fcb953b52292db0e80ba91f2a229361c3b57d12
Iegor Maiorov <maiorov.prog@gmail.com>
2017-05-03 23:57:48 +0300

    * some functions on stream

--- src/main/scala/chapter5/Stream.scala
+++ src/main/scala/chapter5/Stream.scala
@@ -3,22 +3,28 @@ package chapter5
 import scala.annotation.tailrec
 
 trait Stream[+A] {
+
+  def headOption:Option[A] = this match {
+    case Cons(h, t) => Some(h())
+    case Empty => None
+  }
+
   def take(i: Int):Stream[A] = this match {
     case Cons(h,t) if i == 0 => Stream.cons(h(), Stream.empty)
     case Cons(h,t) => Stream.cons(h(), t().take(i - 1))
     case _ => Empty
   }
 
+  def map[B](f: A => B):Stream[B] = foldRight(Empty:Stream[B])((h, t) => Stream.cons(f(h), t))
+
+  def filter(f: A => Boolean):Stream[A] = foldRight(Empty:Stream[A])((h,t) => if (f(h)) Stream.cons(h, t) else t )
+
   def drop(i:Int):Stream[A] = this match {
     case Cons(h, t) if i > 0 => t().drop(i-1)
     case _ => this
   }
 
-  def headOption: Option[A] = this match {
-    case Cons(h, t) => Some(h())
-    case Empty => None
-  }
-
+  def headOptionViaFoldRight():Option[A] = foldRight(None: Option[A])((h, acc) => Some(h))
 
   def toList: List[A] = {
     @tailrec
@@ -29,6 +35,28 @@ trait Stream[+A] {
 
     loop(this, Nil).reverse
   }
+
+  def takeWhile(p: A => Boolean):Stream[A] = this match {
+    case Cons(h,t) if p(h()) => Stream.cons(h(), t().takeWhile(p))
+    case Cons(_,_) => Empty
+    case Empty => Empty
+  }
+
+  def foldRight[B](z:B)(f: (A, => B) => B):B = this match {
+    case Cons(h,t) => f(h(), t().foldRight(z)(f))
+    case Empty => z
+  }
+
+  def flatMap[B](f: A => Stream[B]):Stream[B] = foldRight(Empty:Stream[B])((h,t) => f(h) append t)
+
+  def append[B >: A](s: Stream[B]):Stream[B] = foldRight(s)((h, t) => Stream.cons(h, t) )
+
+  def exists(p: A => Boolean):Boolean = foldRight(false)((a, acc) => p(a) || acc)
+
+  def forAll(p: A => Boolean):Boolean = foldRight(true)((a, acc) => p(a) && acc)
+
+  def takeWhileViaFoldRight(p: A => Boolean):Stream[A] =
+    foldRight(Empty:Stream[A])((a, acc) => if(p(a)) Stream.cons(a, acc) else Empty)
 }
 
 case class Cons[+A](h: () => A, t:() => Stream[A]) extends Stream[A]
--- src/test/scala/chapter5/StreamTest.scala
+++ src/test/scala/chapter5/StreamTest.scala
@@ -20,4 +20,37 @@ class StreamTest extends FunSuite with BeforeAndAfter {
     stream.take(3).toList should contain inOrder (1,2,3)
   }
 
+  test("can take elemnts from stream while condition is true") {
+    stream.takeWhile( _ < 5).toList should contain inOrder(1,2,3,4)
+  }
+
+  test("forall retuns true when specified condition is true for every element in stream") {
+    stream.forAll( _ < 10) shouldBe true
+  }
+
+  test("forall retuns whether specified condition is true") {
+    stream.forAll( _ < 3) shouldBe false
+  }
+
+  test("exist return true when at least one elem in stream satisfy given condition") {
+    stream.exists( _ == 3 ) shouldBe true
+  }
+
+  test("exist return false when at any elem in stream don`t satisfy given condition") {
+    stream.exists( _ == 53 ) shouldBe false
+  }
+
+  test("using map we can map over the stream") {
+    stream.map(_ * 5).toList should contain inOrder (5,10,15,20,25,30)
+  }
+
+  test("using filter we can filter elements from the stream") {
+    stream.filter(_ % 2 != 0).toList should contain inOrder (1,3,5)
+  }
+
+  test("append one stream to other") {
+    val otherStream = Stream(7,8,9,10)
+    stream.append(otherStream).toList should contain inOrder (1,2,3,4,5,6,7,8,9,10)
+  }
+
 }
