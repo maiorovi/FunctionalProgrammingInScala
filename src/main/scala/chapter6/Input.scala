5fb4b0058ceaf6dbcccc7695ef176a5ddc6ac08d
Iegor Maiorov <maiorov.prog@gmail.com>
2017-05-27 23:32:06 +0300

    * implemented state machine however simulateMachine method is purely imperative

--- src/main/scala/chapter6/Input.scala
+++ src/main/scala/chapter6/Input.scala
@@ -9,17 +9,17 @@ case object Turn extends Input
 
 case class Machine(locked: Boolean, candies: Int, coins: Int)
 
-case class State[S, +A](run: S => (A,S)) {
-//  def map[B](f: A => B): State[S, B] = State( state => {
-//    val (value, newState) = run(newState)
-//    (f(value), newState)
-//  })
+case class State[S, +A](run: S => (A, S)) {
+  //  def map[B](f: A => B): State[S, B] = State( state => {
+  //    val (value, newState) = run(newState)
+  //    (f(value), newState)
+  //  })
 
   def map[B](f: A => B): State[S, B] = flatMap(value => State.unit(f(value)))
 
-  def map2[B,C](sb: State[S, B])(f: (A, B) => C): State[S, C] = flatMap(v1 => map(v2 => f(v1, v2)))
+  def map2[B, C](sb: State[S, B])(f: (A, B) => C): State[S, C] = flatMap(v1 => map(v2 => f(v1, v2)))
 
-  def flatMap[B](f: A => State[S, B]): State[S, B] = f((state:A) => {
+  def flatMap[B](f: A => State[S, B]): State[S, B] = f((state: A) => {
     val (value, newState) = run(state)
     value
   })
@@ -27,9 +27,26 @@ case class State[S, +A](run: S => (A,S)) {
 }
 
 object State {
+  def update(input: Input): Machine => Machine = (machine: Machine) => machine match {
+    case Machine(_, 0, _) => machine //if there is no candies ignore all inputs
+    case Machine(false, candies, coins) => input match {
+      case Coin => machine //inserting a coint ot unlocked machine does nothing
+      case Turn => Machine(true, candies -1, coins) //
+    }
+      //inserting a coin
+    case Machine(true, candies, coins) => input match {
+      case Coin => Machine(false, candies, coins + 1) //unlock machine
+      case Turn => machine // turning the knob on locked machine does nothing
+    }
+  }
 
-  def simulateMachine(inputs: List[Input]):State[Machine, (Int, Int)] = ???
+  def simulateMachine(inputs: List[Input]): State[Machine, (Int, Int)] = {
+    State( machine => {
+      inputs.foreach(inp => update(inp)(machine))
+      ((machine.candies, machine.coins),machine)
+    } )
+  }
 
-  def unit[S,A](a:A):State[S, A] = State(state => (a, state))
+  def unit[S, A](a: A): State[S, A] = State(state => (a, state))
 
 }
